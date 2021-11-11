4d34037bf741bb16f24234cf5a6200b1b54c5fbc
Rahul Jaisimha <rahul@jaisimha.com>
2021-11-11 18:28:04 +0530

    Added awards

--- js/app.js
+++ js/app.js
@@ -7,6 +7,12 @@ class Header extends React.Component {
             <div id='title'>Aditi Verma</div>
             <div id='subtitle'>is a communications designer excited by all things 'text & image'.</div>
             <div id='subtitle'>She enjoys building design systems with multidisciplinary teams.</div>
+
+            <div id='awards'>
+                <div id='award'>AIGA NY Grad 2019</div>
+                <div id='award'>GDUSA American Graphic Design Award 2019</div>
+            </div>
+
             <div id='contact'>CONTACT</div>
             </div>
         )
